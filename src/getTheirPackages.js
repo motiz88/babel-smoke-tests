5d6543df5418257eaa4805ed40b1d23a4a767b9a
Moti Zilberman <motiz88@gmail.com>
2016-10-06 23:29:53 +0300

    Skip symlinks in "their" packages

--- src/getTheirPackages.js
+++ src/getTheirPackages.js
@@ -1,5 +1,11 @@
 var glob = require('glob');
 var path = require('path');
+var fs = require('fs');
+
+function isSymlink (filename) {
+  var stats = fs.lstatSync(filename);
+  return stats && stats.isSymbolicLink();
+}
 
 module.exports = function getTheirPackages () {
   var packageJsonLocations = glob.sync("**/package.json", {
@@ -8,8 +14,12 @@ module.exports = function getTheirPackages () {
   });
   return packageJsonLocations.map(function (packageJsonLocation) {
     try {
+      var location = path.dirname(packageJsonLocation);
+      if (isSymlink(location)) {
+        return null;
+      }
       var packageJson = require(packageJsonLocation);
-      return {name: packageJson.name, location: path.dirname(packageJsonLocation)};
+      return {name: packageJson.name, location: location};
     } catch (e) {
       return null;
     }
