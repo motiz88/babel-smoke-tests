29561ebd18bd819ff59af83fed57256fb6a5c1be
Moti Zilberman <motiz88@gmail.com>
2016-10-06 23:22:16 +0300

    Fix patchDependency invocation

--- scripts/patch-deps.js
+++ scripts/patch-deps.js
@@ -5,5 +5,6 @@ var getTheirPackages = require('../src/getTheirPackages');
 var getDepsToOverride = require('../src/getDepsToOverride');
 var patchDependency = require('../src/patchDependency');
 
-var deps = getDepsToOverride(getTheirPackages(), getOurPackages());
-deps.forEach(patchDependency);
+var ourPackages = getOurPackages();
+var deps = getDepsToOverride(getTheirPackages(), ourPackages);
+deps.forEach(function (dep) { patchDependency(dep, ourPackages); });
