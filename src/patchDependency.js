0ac3842ffc7a97776bb42613f89b57bb5b401545
Moti Zilberman <motiz88@gmail.com>
2016-10-06 23:22:05 +0300

    Show output of npm link

--- src/patchDependency.js
+++ src/patchDependency.js
@@ -2,5 +2,5 @@ var spawn = require('cross-spawn');
 
 module.exports = function patchDependency (dependencyRecord, dependencyPoolMap) {
   console.log('patching ', dependencyRecord.location, ' for ', dependencyRecord.name);
-  spawn.sync("npm", ["link", dependencyPoolMap[dependencyRecord.name].location], {cwd: dependencyRecord.location});
+  spawn.sync("npm", ["link", dependencyPoolMap[dependencyRecord.name].location], {cwd: dependencyRecord.location, stdio: "inherit"});
 };
