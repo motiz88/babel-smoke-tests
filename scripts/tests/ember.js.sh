bd5dfa2154e90ea8f3280c4cdd9ec32730534a71
Moti Zilberman <motiz88@gmail.com>
2016-10-07 17:07:32 +0300

    Specify some env vars for Ember build

--- scripts/tests/ember.js.sh
+++ scripts/tests/ember.js.sh
@@ -11,9 +11,10 @@ pushd $THEM/ember.js
 npm install -g bower
 npm install
 bower install
+BROCCOLI_ENV=production npm run build
 $END 'Done building Ember.js' tests.ember.js.build
 $START 'Testing Ember.js' tests.ember.js.test
-npm test
+TEST_SUITE=each-package-tests DISABLE_JSCS=true DISABLE_JSHINT=true BROCCOLI_ENV=production npm test
 popd
 
 $END 'Done testing Ember.js' tests.ember.js.test
