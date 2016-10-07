f1c80220fc4246c4adf60dfe7ee224bede2b1ddd
Moti Zilberman <motiz88@gmail.com>
2016-10-07 05:39:31 +0300

    Run npm whoami in bootstrap for logging

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,11 +6,13 @@ JQ=$(pwd)/node_modules/.bin/jq
 pushd $US
 pushd babylon
 npm install
+npm whoami
 npm publish
 popd # babylon
 pushd babel
 make bootstrap
 BABEL_ENV=production make build-dist
+npm whoami
 node_modules/.bin/lerna publish --only-explicit-updates --skip-git --yes --repo-version $(JQ -r .version lerna.json)
 popd # babel
 popd # $US
