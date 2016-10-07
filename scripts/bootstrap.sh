1d17b466a64a741b8f4c5c8b456b1dbf38a3bb20
Moti Zilberman <motiz88@gmail.com>
2016-10-07 06:36:49 +0300

    Bump versions before fake-publishing

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,15 +4,18 @@ set -e
 
 US=$(cd us; pwd)
 JQ=$(pwd)/node_modules/.bin/jq
+SEMVER=$(pwd)/node_modules/.bin/SEMVER
 
 pushd $US
 pushd babylon
 npm install
+npm version patch
 npm publish
 popd # babylon
 pushd babel
 make bootstrap
 BABEL_ENV=production make build-dist
-node_modules/.bin/lerna publish --only-explicit-updates --skip-git --yes --repo-version $(JQ -r .version lerna.json)
+BABEL_REPO_VERSION=$(JQ -r .version lerna.json)
+node_modules/.bin/lerna publish --only-explicit-updates --skip-git --yes --repo-version $(SEMVER -i $BABEL_REPO_VERSION)
 popd # babel
 popd # $US
