7ea1bcc35dd07b7e0bd4676e1ebba7d0ea6165c5
Moti Zilberman <motiz88@gmail.com>
2016-10-07 06:25:41 +0300

    Set NPM_CONFIG_REGISTRY

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,7 @@ node_modules/.bin/verdaccio -l localhost:4873 -c verdaccio.yml &
 
 REGISTRY=http://localhost:4873/
 npm config --global set registry $REGISTRY --loglevel silly
+export NPM_CONFIG_REGISTRY=$REGISTRY
 npm config list
 
 NPM_LOGIN=$(pwd)/scripts/npm-login.sh
