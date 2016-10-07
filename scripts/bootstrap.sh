da2fcae98a11a1efd533e11d4c0bfff878b7fcc5
Moti Zilberman <motiz88@gmail.com>
2016-10-07 08:27:44 +0300

    Switch to lerna --force-publish

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -16,6 +16,6 @@ pushd babel
 make bootstrap
 BABEL_ENV=production make build-dist
 BABEL_REPO_VERSION=`$JQ -r .version lerna.json`
-node_modules/.bin/lerna publish --only-explicit-updates --skip-git --yes --repo-version `$SEMVER -i $BABEL_REPO_VERSION`
+node_modules/.bin/lerna publish --force-publish=* --skip-git --yes --repo-version `$SEMVER -i $BABEL_REPO_VERSION`
 popd # babel
 popd # $US
