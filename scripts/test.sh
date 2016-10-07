cf0d1ce9986c2d5c0e5e5c661a2c7980445d05a9
Moti Zilberman <motiz88@gmail.com>
2016-10-07 05:00:47 +0300

    Add a dummy npm user to the local repository

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,12 @@ node_modules/.bin/verdaccio -l localhost:4873 -c verdaccio.yml &
 
 npm set registry http://localhost:4873/
 
+npm adduser <<EOF
+smoketest
+smoketest
+smoketest@babeljs.io
+EOF
+
 scripts/bootstrap.sh
 
 THEM=$(cd them; pwd)
