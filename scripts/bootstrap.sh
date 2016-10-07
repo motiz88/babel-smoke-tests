15f173b41d8cc7ab41fac3b2e2620272b811208c
Moti Zilberman <motiz88@gmail.com>
2016-10-07 04:48:16 +0300

    Force feed a version to lerna publish

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 US=$(cd us; pwd)
+JQ=$(pwd)/node_modules/jq
 
 pushd $US
 pushd babylon
@@ -10,6 +11,6 @@ popd # babylon
 pushd babel
 make bootstrap
 BABEL_ENV=production make build-dist
-./node_modules/.bin/lerna publish --only-explicit-updates --skip-git --yes
+node_modules/.bin/lerna publish --only-explicit-updates --skip-git --yes --repo-version $(JQ -r .version lerna.json)
 popd # babel
 popd # $US
